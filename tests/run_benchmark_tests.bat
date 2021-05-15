493108f661ed2bcef74a09b2ab89ce2fbfd1c3d3
Alexander Martynov <amart@mail.ru>
2021-05-16 02:42:34 +0300

    Added generated test inputs and benchmark results ti the doc folder

--- tests/run_benchmark_tests.bat
+++ tests/run_benchmark_tests.bat
@@ -1,2 +1,12 @@
 @call run_test.bat benchmark_tests msvc2017 x86 Release
-@rem call run_test.bat benchmark_tests msvc2017 x64 Release
+@copy _run_logs\benchmark_tests.log  doc\benchmark_tests.txt
+
+@call run_test.bat benchmark_tests msvc2017 x64 Release
+@echo >> doc\benchmark_tests.txt
+@echo >> doc\benchmark_tests.txt
+@echo >> doc\benchmark_tests.txt
+@echo --------------------------------------------------------------------------------------------------------------------------------------------------- >> doc\benchmark_tests.txt
+@echo >> doc\benchmark_tests.txt
+@echo >> doc\benchmark_tests.txt
+@echo >> doc\benchmark_tests.txt
+@type _run_logs\benchmark_tests.log  >> doc\benchmark_tests.txt
