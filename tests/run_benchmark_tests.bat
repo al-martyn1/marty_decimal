7b312b58f0dfa1b8c57b5af87b568a4df4a4e3e2
alexander_martynov <a.martynov@rtc.ru>
2021-05-17 15:10:09 +0300

    Removed single test results, added test results performed on work PC

--- tests/run_benchmark_tests.bat
+++ tests/run_benchmark_tests.bat
@@ -1,12 +1,12 @@
 @call run_test.bat benchmark_tests msvc2017 x86 Release
-@copy _run_logs\benchmark_tests.log  doc\benchmark_tests.txt
+@copy /Y _run_logs\benchmark_tests.log  doc\benchmark_tests.txt
 
 @call run_test.bat benchmark_tests msvc2017 x64 Release
-@echo. >> doc\benchmark_tests.txt
-@echo. >> doc\benchmark_tests.txt
-@echo. >> doc\benchmark_tests.txt
-@echo --------------------------------------------------------------------------------------------------------------------------------------------------- >> doc\benchmark_tests.txt
-@echo. >> doc\benchmark_tests.txt
-@echo. >> doc\benchmark_tests.txt
-@echo. >> doc\benchmark_tests.txt
+@rem echo. >> doc\benchmark_tests.txt
+@rem echo. >> doc\benchmark_tests.txt
+@rem echo. >> doc\benchmark_tests.txt
+@rem echo --------------------------------------------------------------------------------------------------------------------------------------------------- >> doc\benchmark_tests.txt
+@rem echo. >> doc\benchmark_tests.txt
+@rem echo. >> doc\benchmark_tests.txt
+@rem echo. >> doc\benchmark_tests.txt
 @type _run_logs\benchmark_tests.log  >> doc\benchmark_tests.txt
